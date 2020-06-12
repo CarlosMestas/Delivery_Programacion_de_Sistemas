841f3104c2a668fb85c7bf336836549c3453616e
Carlos Alberto Mestas Escarcena <cmestas>
2020-06-12 16:27:28 -0500

    First commit

---
+++ ConsoleApplication1/Test_Conexion.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mysql.h>
+int main() {
+	MYSQL* conn;
+	MYSQL_RES* res;
+	MYSQL_ROW row;
+	const char* server = "programacionsistemasproyectodelivery.mysql.database.azure.com";
+	const char* user = "cmestas@programacionsistemasproyectodelivery";
+	const char* password = "PS_123**";
+	const char* database = "delivery";
+	conn = mysql_init(NULL);
+	if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)) {
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+	if (mysql_query(conn, "select * from Cliente")) {
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+	res = mysql_use_result(conn);
+	printf("Tabla clientes:\n");
+	while ((row = mysql_fetch_row(res)) != NULL) 
+		printf("%s \t %s \t %s \n", row[0], row[1], row[2]);
+	mysql_free_result(res);
+	mysql_close(conn);
+	return 0;
+}
